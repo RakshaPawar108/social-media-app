4cb44fe509970ec325946a6befe58b3f0da78988
Raksha Pawar <rakshapawar10.sae@gmail.com>
2022-07-09 13:51:55 +0530

    feat: add login route

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import { Login } from "./pages";
 
 function App() {
-  return <div className="App">Welcome to Socioverse</div>;
+  return (
+    <div className="App">
+      Welcome to Socioverse
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
