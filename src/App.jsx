59472181d7d5c9293a1aa9ec20202ec167f1581d
Raksha Pawar <rakshapawar10.sae@gmail.com>
2022-07-17 10:40:46 +0530

    feat: add dark mode along with theme changing button

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import { ThemeButton } from "./components/ThemeButton/ThemeButton";
 import { Login, Signup } from "./pages";
 
 function App() {
   return (
     <div className="App">
       Welcome to Socioverse
+      <ThemeButton />
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import { BrowserRouter as Router } from "react-router-dom";
+import theme from "./theme/theme";
 
 // Call make Server
 makeServer();
@@ -14,7 +15,8 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Router>
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <App />
       </ChakraProvider>
     </Router>
