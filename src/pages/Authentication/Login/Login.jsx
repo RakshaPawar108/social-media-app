b5c5437eab99032a5817fecc17cab67e69969c41
Raksha Pawar <rakshapawar10.sae@gmail.com>
2022-07-17 13:18:06 +0530

    style: adjusting login and signup page styles according to reset

--- src/pages/Authentication/Login/Login.jsx
+++ src/pages/Authentication/Login/Login.jsx
@@ -16,44 +16,63 @@ import { Link as RouteLink } from "react-router-dom";
 export const Login = () => {
   return (
     <Center>
-      <Container maxW={320}>
+      <Container maxW={340}>
         <Box
           width="full"
           height="100vh"
           justifyContent="center"
           flexDirection="column"
           display="flex"
-          gap="1.5rem"
+          gap="2.5rem"
+          fontSize="1.5rem"
         >
-          <Text mb="1rem">
-            <Heading size="md" p="0.5rem">
+          <Text mb="1rem" fontSize="1.5rem">
+            <Heading size="xl" p="0.5rem">
               LOGIN
             </Heading>
             Please enter your credentials
           </Text>
           <FormControl>
-            <FormLabel>Username</FormLabel>
+            <FormLabel fontSize="1.5rem" pb="1rem">
+              Username
+            </FormLabel>
             <Input
               id="username"
               type="text"
               variant="flushed"
               placeholder="IronMan3000"
+              fontSize="1.5rem"
             />
           </FormControl>
           <FormControl>
-            <FormLabel>Password</FormLabel>
+            <FormLabel fontSize="1.5rem" pb="1rem">
+              Password
+            </FormLabel>
             <Input
               id="password"
               type="password"
               variant="flushed"
               placeholder="********"
+              fontSize="1.5rem"
             />
           </FormControl>
           <Checkbox spacing="1rem">Remember Me</Checkbox>
-          <Button colorScheme="teal" variant="solid" w="full">
+          <Button
+            colorScheme="teal"
+            variant="solid"
+            w="full"
+            fontSize="1.5rem"
+            p="1.5rem"
+          >
             LOGIN
           </Button>
-          <Button colorScheme="teal" variant="solid" w="full">
+          <Button
+            colorScheme="teal"
+            variant="solid"
+            w="full"
+            fontSize="1.5rem"
+            p="1.5rem"
+          >
             LOGIN WITH GUEST CREDENTIALS
           </Button>
           <Text>
--- src/pages/Authentication/Signup/Signup.jsx
+++ src/pages/Authentication/Signup/Signup.jsx
@@ -24,64 +24,86 @@ export const Signup = () => {
           justifyContent="center"
           flexDirection="column"
           display="flex"
-          gap="1.5rem"
+          gap="2.5rem"
+          fontSize="1.5rem"
         >
-          <Text mb="1rem">
-            <Heading size="md" p="0.5rem">
+          <Text mb="1rem" fontSize="1.5rem">
+            <Heading size="xl" p="0.5rem">
               SIGNUP
             </Heading>
             Create an Account
           </Text>
           <Flex gap="0.5rem">
             <FormControl>
-              <FormLabel>First Name</FormLabel>
+              <FormLabel fontSize="1.5rem" pb="1rem">
+                First Name
+              </FormLabel>
               <Input
                 id="firstname"
                 type="text"
                 variant="flushed"
                 placeholder="Jane"
+                fontSize="1.5rem"
               />
             </FormControl>
             <FormControl>
-              <FormLabel>Last Name</FormLabel>
+              <FormLabel fontSize="1.5rem" pb="1rem">
+                Last Name
+              </FormLabel>
               <Input
                 id="lastname"
                 type="text"
                 variant="flushed"
                 placeholder="Fonda"
+                fontSize="1.5rem"
               />
             </FormControl>
           </Flex>
 
           <FormControl>
-            <FormLabel>Username</FormLabel>
+            <FormLabel fontSize="1.5rem" pb="1rem">
+              Username
+            </FormLabel>
             <Input
               id="username"
               type="text"
               variant="flushed"
               placeholder="IronMan3000"
+              fontSize="1.5rem"
             />
           </FormControl>
           <FormControl>
-            <FormLabel>Password</FormLabel>
+            <FormLabel fontSize="1.5rem" pb="1rem">
+              Password
+            </FormLabel>
             <Input
               id="password"
               type="password"
               variant="flushed"
               placeholder="********"
+              fontSize="1.5rem"
             />
           </FormControl>
           <FormControl>
-            <FormLabel>Confirm Password</FormLabel>
+            <FormLabel fontSize="1.5rem" pb="1rem">
+              Confirm Password
+            </FormLabel>
             <Input
               id="confirmPassword"
               type="password"
               variant="flushed"
               placeholder="********"
+              fontSize="1.5rem"
             />
           </FormControl>
           <Checkbox spacing="1rem">Accept Terms and Conditions</Checkbox>
-          <Button colorScheme="teal" variant="solid" w="full">
+          <Button
+            colorScheme="teal"
+            variant="solid"
+            w="full"
+            fontSize="1.5rem"
+            p="1.5rem"
+          >
             SIGNUP
           </Button>
           <Text>
