aeaed6315c3d00f6691be81042be58cd5c4ae560
Raksha Pawar <rakshapawar10.sae@gmail.com>
2022-07-24 13:35:39 +0530

    feat: remove sidebar when login or signup route is seen

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,34 @@
-import { Route, Routes } from "react-router-dom";
+import { Container } from "@chakra-ui/react";
+import { Route, Routes, useLocation } from "react-router-dom";
 import "./App.css";
 import { SideNav, ThemeButton } from "./components";
 import { Login, Signup } from "./pages";
 
 function App() {
+  const { pathname } = useLocation();
   return (
-    <div className="App">
+    <Container
+      className="App"
+      as="main"
+      maxW="container.xl"
+      display="flex"
+      mx="auto"
+      justifyContent={
+        pathname.slice(1) === "login" || pathname.slice(1) === "signup"
+          ? "center"
+          : "unset"
+      }
+    >
       <ThemeButton />
-      <SideNav />
+      {pathname.slice(1) === "login" ||
+      pathname.slice(1) === "signup" ? null : (
+        <SideNav />
+      )}
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
-    </div>
+    </Container>
   );
 }
 
